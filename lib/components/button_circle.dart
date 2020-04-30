e4c310a34927e26e3ed44c10123b5b43a57c04d8
lyabs243 <loicyabili@outlook.com>
2020-04-30 16:05:08 +0200

    Create a circle button component

---
+++ lib/components/button_circle.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ButtonCircle extends StatelessWidget {
+
+  Icon icon;
+  Color borderColor, fillColor;
+  Function onPress;
+
+  ButtonCircle(this.icon, this.onPress, {this.borderColor: Colors.blue, this.fillColor: Colors.black});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPress,
+      elevation: 2.0,
+      fillColor: this.fillColor,
+      child: this.icon,
+      padding: EdgeInsets.all(15.0),
+      shape: CircleBorder(side: BorderSide(color: this.borderColor, width: 4.0)),
+    );
+  }
+
+}
