6f5a0b9e283434be8049fe60335c2d0d116b1eb6
lyabs243 <loicyabili@outlook.com>
2020-05-06 12:11:28 +0200

    Create class to get data from server

---
+++ lib/services/api.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class Api {
+
+  int statusCode;
+  Map mapResult;
+  BuildContext context;
+
+  Api(this.context);
+
+  Future<Map> getJsonFromServer(String url, Map<String,dynamic> params) async {
+    Map<String,dynamic> map = {
+      'api_key': "xz]nNQz%mX+g<*Rd,*eA4'0w~EYs9N",
+    };
+    //print("params -- ${params}");
+    if (params != null) {
+      map.addAll(params);
+    }
+
+    try{
+      FormData formData = new FormData.fromMap(map);
+      final response = await Dio().post(url, data: formData);
+      this.statusCode = response.statusCode;
+      if (response.statusCode == 200) {
+          //print(response.data.toString());
+          mapResult = response.data;
+          //print(mapResult['NOTIFYGROUP']);
+      }
+    }
+    catch(e){
+      //print(e);
+    }
+
+    return mapResult;
+  }
+
+}
